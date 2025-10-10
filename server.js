67a101a1a79409056fa40bfa5e03ed7b40837312
bhowmik94 <sourav.bhowmik@stud.uni-bamberg,de>
2025-10-10 14:15:53 +0200

    change port to 10000, added start command in package json scripts

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ app.use(cookieParser());
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/posts", require("./routes/postRoutes"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
