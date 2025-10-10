9768ee9c655e054b410160c9c00c1ac97d06149e
bhowmik94 <sourav.bhowmik@stud.uni-bamberg,de>
2025-10-10 19:09:16 +0200

    mongo atlas issue fixed locally

--- server.js
+++ server.js
@@ -12,9 +12,5 @@ app.use(cookieParser());
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/posts", require("./routes/postRoutes"));
 
-app.get("/", (req, res) => {
-  res.send("✅ Server is live and working");
-});
-
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
